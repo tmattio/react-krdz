3a7e5288b57a785c9c26b6cdf30ed044d5e488e0
Thibaut Mattio <thibaut.mattio@gmail.com>
2016-12-09 21:47:45 -0500

    Adding an header to the App component, it looks nicer.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Board from './Kanban/Board'
+import Header from './Header'
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Header />
         <Board id={'F4sP3vRt'} />
       </div>
     );
---
+++ src/Header.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Navbar, Nav, NavItem, MenuItem, NavDropdown } from 'react-bootstrap';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar inverse fixedTop>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">Krdz Proto.</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <NavItem eventKey={1} href="#">Link Right</NavItem>
+              <NavItem eventKey={2} href="#">Link Right</NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default Header;
