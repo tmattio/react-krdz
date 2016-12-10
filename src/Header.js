7ee9e588a5644662aaa729b9dd057d414c484de1
Thibaut Mattio <thibaut.mattio@gmail.com>
2016-12-09 21:48:27 -0500

    Removing unused import in the header

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Navbar, Nav, NavItem, MenuItem, NavDropdown } from 'react-bootstrap';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
 class Header extends Component {
   render() {
