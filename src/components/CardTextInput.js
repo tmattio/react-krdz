7ee5fde63bb3ec3457b96f160b8d5730b1657664
Thibaut Mattio <thibaut.mattio@gmail.com>
2016-12-12 04:22:01 -0500

    Reimplements the onBlur in CardTextInput
    This was actually useful to save the changes when clicking elsewhere

--- src/components/CardTextInput.js
+++ src/components/CardTextInput.js
@@ -23,6 +23,10 @@ export default class CardTextInput extends Component {
     this.setState({ text: e.target.value })
   }
 
+  handleBlur = e => {
+    this.props.onSave(e.target.value)
+  }
+
   render() {
     return (
       <input className={
@@ -34,8 +38,8 @@ export default class CardTextInput extends Component {
         placeholder={this.props.placeholder}
         autoFocus="true"
         value={this.state.text}
-        onBlur={this.handleBlur}
         onChange={this.handleChange}
+        onBlur={this.handleBlur}
         onKeyDown={this.handleSubmit} />
     )
   }
