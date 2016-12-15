614b87a20aabe6c2be4d89f2e47fe04cf950dc6e
Thibaut Mattio <thibaut.mattio@gmail.com>
2016-12-14 23:05:07 -0500

    Change App Component to be a stateless functions
    See: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prefer-stateless-function.md

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,14 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 
-class App extends React.PureComponent {
-  render() {
-    // NOTE: If you need to add or modify header, footer etc. of the app,
-    // please do that inside the Layout component.
-    return (
-      <div>
-        {React.Children.only(this.props.children)}
-      </div>
-    );
-  }
-}
+// eslint-disable-next-line func-names
+const App = function (props) {
+  return (
+    <div>
+      {React.Children.only(props.children)}
+    </div>
+  );
+};
 
 App.propTypes = {
   // Injected by React Router
